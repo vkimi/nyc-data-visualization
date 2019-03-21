f556f3345a482edc0811f9a85b674237de4a26d6
Kim <kimv@sfu.ca>
2019-03-21 14:24:43 -0700

    Set up pie structure

--- js/main.js
+++ js/main.js
@@ -20,3 +20,34 @@ DataFrame.fromCSV('https://gist.githubusercontent.com/justinhodev/67f817e910a0de
     .catch(err => {
         console.log(err);
 });
+DataFrame.fromCSV('https://gist.githubusercontent.com/justinhodev/bbe43324cbf228095e47bb471e5930f2/raw/632f57b671376fa8dcc3a8d5755e3004204da326/nyc_census_tracts.csv?fbclid=IwAR388bG7eeMcRqNicntr3lXPsMrxjKLW_egXE5XXq6VxL_F0ZCuskrGe75I')
+    .then( df2 => {
+        // filter out 'Region Total' rows skewing data
+        df3 = df2.replace(``, `0`) 
+        var factors = df3.filter(row => row.get(df3.listColumns()[0]) != 'Region Total');
+
+        // convert to JSON object
+        factors = factors.toCollection();
+        // setup visualization
+        var view = render('#nyc-pie', nyc_category);
+
+        var asianTotal= 0; //create a new array that looks like my values on the left side
+                            // how do I bring this value over?
+        for (i = 0; i < factors.length-1; i++) { 
+            if(typeof parseFloat(factors[i]['Asian']) == 'number'){ 
+                asianTotal += parseFloat(factors[i]['Asian']);
+            }else{
+                console.log(factors[i]['Asian'] + " is not a number <br/>");
+            }
+        }
+
+
+        // insert wrangled dataset at runtime
+       view.change('nyc-pie-data', vega.changeset().insert(factors));
+
+        // load graph
+        view.runAsync();
+    })
+    .catch(err => {
+        console.log(err);
+});
